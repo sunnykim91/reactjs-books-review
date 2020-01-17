95e0d36eaa007bd4c4f860f24f8883de9b972c8a
sunnykim91 <tjsgnl2002@gmail.com>
2020-01-17 21:10:02 +0900

    git init & settings

---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => {
+  return <div>Home입니다.</div>;
+};
+
+export default Home;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NotFound = () => {
+  return <div>NotFound입니다.</div>;
+};
+
+export default NotFound;
